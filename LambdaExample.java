4ffe2f26a4f658c9db4411337f99457a62d9223c
Nikitha-prog-cmd <varmanikitha595@gmail.com>
2025-05-30 16:25:21 +0530

    Add files via upload

---
+++ LambdaExample.java
@@ -0,0 +1,14 @@
+package xyz;
+
+interface message{
+	void say();
+}
+public class LambdaExample {
+
+	public static void main(String[] args) {
+		message hellomessage=()->System.out.println("This is Lambda Example");
+		hellomessage.say();
+
+	}
+
+}
